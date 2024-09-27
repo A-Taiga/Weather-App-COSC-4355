ce7e3d75d9fde9dfbcf4fd4652ce9644ef8e1bfc
Anthony Polka <anthonypolka@gmail.com>
2024-09-27 14:00:27 -0500

    added dummy data, weather codables, fileReader

---
+++ Weather-App/Utility.swift
@@ -0,0 +1,8 @@
+//
+//  Utility.swift
+//  Weather-App
+//
+//  Created by Anthony Polka on 9/27/24.
+//
+
+import Foundation
---
+++ Weather-App/WeatherAPI.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherAPI.swift
+//  Weather-App
+//
+//  Created by Anthony Polka on 9/27/24.
+//
+
+import Foundation
