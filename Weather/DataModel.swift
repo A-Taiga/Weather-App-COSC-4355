889dc4702288dd90d551da1791102e050708acb9
Anthony Polka <anthonypolka@gmail.com>
2024-10-30 12:16:25 -0500

    added seperate model for location information

--- Weather/DataModel.swift
+++ Weather/DataModel.swift
@@ -7,16 +7,43 @@
 
 import Foundation
 import SwiftData
+import CoreLocation
+
+
 
 @Model
-final class DataModel: Identifiable, Hashable {
+final class DataModel: Identifiable {
+    
     var id = UUID()
-    @Attribute(.unique) var name: String
+    var location: LocationModel
     var listIndex: Int
     var weatherData: WeatherData
-    init(name: String, weatherData: WeatherData, listIndex: Int) {
-        self.name = name
+    init(location: LocationModel, weatherData: WeatherData, listIndex: Int) {
+        self.location = location
         self.weatherData = weatherData
         self.listIndex = listIndex
+    }
+
+}
+
+@Model
+final class LocationModel: Equatable {
+
+    var locality: String
+    var administrativeArea: String
+    var subAdministrativeArea: String
+    var lat: Double
+    var lon: Double
+    
+    init(locality: String, administrativeArea: String, subAdministrativeArea: String, coordinates: CLLocationCoordinate2D) {
+        self.locality = locality
+        self.administrativeArea = administrativeArea
+        self.subAdministrativeArea = subAdministrativeArea
+        self.lat = coordinates.latitude
+        self.lon = coordinates.longitude
+    }
+    
+    static func ==(lhs: LocationModel, rhs: LocationModel) -> Bool {
+        return lhs.locality == rhs.locality && lhs.administrativeArea == rhs.administrativeArea
     }
 }
